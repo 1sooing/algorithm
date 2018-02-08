f42af8d89a4f71ad190ba5d03e6f7544eb80bac0
1sooing <1sooing@gmail.com>
2018-02-09 00:31:11 +0900

    Solved Q2018

--- BJOJ_Q2018.cpp
+++ BJOJ_Q2018.cpp
@@ -1,8 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main()
 {
+	int N, i, tmp, ANS=1;
 
+	scanf("%d", &N);
 
+	for (i = 2; i < N; i++) {
+
+		// case of odd
+		if ( i % 2 ) {
+			if ( (N%i == 0) && (N/i - i/2 > 0) )
+				ANS++;
+		}
+
+		// case of even
+		else {
+			tmp = N / i;
+			if (tmp - i / 2 > -1)
+				if ((2 * tmp + 1) * i / 2 == N)
+					ANS++;
+		}
+	}
+	printf("%d\n", ANS);
     return 0;
 }
