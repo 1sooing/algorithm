2c6d455a23a3b0d5735cf063076084cc4e4ce330
1sooing <1sooing@gmail.com>
2018-02-09 11:17:10 +0900

    First try

--- BJOJ_Q1920.cpp
+++ BJOJ_Q1920.cpp
@@ -1,10 +1,32 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+vector<int> ARR;
+
+bool compare(int i, int j) { return (i < j); }
 
 int main() {
 
+	int i, j, N, M, QUESTION, ANS;
+	
+	for (cin>>N, i = 0; i < N; i++) {
+		cin >> M;
+		ARR.push_back(M);
+	}
 
+	sort(ARR.begin(), ARR.end());
 
+	for (cin>>M; M--;) {
+		
+		cin >> QUESTION;
+
+		ANS = (binary_search(ARR.begin(), ARR.end(), QUESTION, compare)) ? 1 : 0;
+
+		printf("%d\n", ANS);
+	}
 
 	return 0;
 }
